93b0c524a703ba4449c1efb059b022ca8199edf2
Manishbhai9350 <keshraram294@gmail.com>
2025-11-26 20:54:56 +0530

    ASCII Characters Implementation

--- src/ascii/index.js
+++ src/ascii/index.js
@@ -1,12 +1,77 @@
-import { vec4 } from "three/tsl"
+import {
+  float, vec2, vec3, vec4,
+  color, texture, uniform,
+  positionLocal, normalLocal,
+  uv, time,
+  attribute,
+  Fn,
+  pow,
+  floor,
+  lengthSq,
+  mix,
+  step
+} from 'three/tsl';
+
 import { MeshBasicNodeMaterial } from "three/webgpu"
 
+const ColorPallete = [
+    '#FAF3E1',
+    '#D7C097',
+    '#4E56C0',
+    '#FF6D1F',
+    '#961E1E',
+]
 
 
-export const getMaterial = () => {
+
+export const getMaterial = ({
+    invr=1,
+    invc=1,
+    person,
+    ascii,
+    length
+}) => {
     const material = new MeshBasicNodeMaterial()
 
-    material.colorNode = vec4(1,.6,0,.5)
+    const color1 = uniform(color(ColorPallete[0]))
+    const color2 = uniform(color(ColorPallete[2]))
+    const color3 = uniform(color(ColorPallete[2]))
+    const color4 = uniform(color(ColorPallete[3]))
+    const color5 = uniform(color(ColorPallete[4]))
+
+    
+    material.colorNode = Fn(() => {
+    const RawScreenUV = attribute('screenUV', 'vec2');
+        const rows = attribute('rows', 'float');
+        const columns = attribute('column', 'float');
+        
+        const invRows = float(1).div(rows);
+        const invColumns = float(1).div(columns);
+    
+        const ScreenUV = RawScreenUV.add(uv().mul(vec2(invc,invr)))
+
+        const Person = texture(person,RawScreenUV)
+
+        const Brightness = pow(Person.r,.45)
+        let finalColor = color1;
+        finalColor = mix(finalColor,color2,step(.2,Brightness))
+        finalColor = mix(finalColor,color3,step(.4,Brightness))
+        finalColor = mix(finalColor,color4,step(.6,Brightness))
+        finalColor = mix(finalColor,color5,step(.8,Brightness))
+
+        const asciiUV = vec2(
+            uv().x.
+            div(float(length))
+            .add(floor(pow(Person.r,1.5).mul(length)).div(length)),
+            uv().y)
+        const asciiTexture = texture(ascii,asciiUV)
+
+        
+
+        // return vec4(Brightness)
+        return asciiTexture.mul(finalColor)
+        // return vec4(vec3(finalColor),1)
+    })()
 
     return material
 }
--- src/main.js
+++ src/main.js
@@ -1,97 +1,134 @@
-import './style.css'
-import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
-import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader'
-import fragmentShader from './shaders/fragment.glsl'
-import vertexShader from './shaders/vertex.glsl'
-import { PlaneGeometry, WebGPURenderer } from 'three/webgpu';
-import { GetSceneBounds } from './utils';
-import { getMaterial } from './ascii'
+import "./style.css";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import fragmentShader from "./shaders/fragment.glsl";
+import vertexShader from "./shaders/vertex.glsl";
+import { PlaneGeometry, WebGPURenderer } from "three/webgpu";
+import { GetSceneBounds } from "./utils";
+import { getMaterial } from "./ascii";
+import Person from "/person.jpg?url";
 
+const { PI } = Math;
 
-const {PI} = Math
-
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector("canvas");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-const renderer = new WebGPURenderer({canvas,antialias:true,alpha:true})
+const renderer = new WebGPURenderer({ canvas, antialias: true, alpha: true });
 
-const camera = new THREE.PerspectiveCamera(75,innerWidth/innerHeight,1,1000)
-camera.position.z = 5
-
-
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  1,
+  1000
+);
+camera.position.z = 5;
 
 const Manager = new THREE.LoadingManager();
-const Draco = new DRACOLoader(Manager)
-const GLB = new GLTFLoader(Manager)
+const Draco = new DRACOLoader(Manager);
+const Texture = new THREE.TextureLoader(Manager);
+const GLB = new GLTFLoader(Manager);
 
-Draco.setDecoderPath('/draco/')
-Draco.setDecoderConfig({type: 'wasm'})
-GLB.setDRACOLoader(Draco)
+Draco.setDecoderPath("/draco/");
+Draco.setDecoderConfig({ type: "wasm" });
+GLB.setDRACOLoader(Draco);
 
-const {width,height} = GetSceneBounds(renderer,camera)
+const { width, height } = GetSceneBounds(renderer, camera);
 
-let size = .2;
+const GetASCIITexture = () => {
+  let ascii = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
+  const length = ascii.length;
 
-const row = Math.ceil(height / (size)) + 1
-const column = Math.ceil(width / (size )) + 1
-const instances = row * column
+  // document.querySelector('canvas').style.display = 'none'
+  const canvas = document.createElement('canvas')
+  // document.body.querySelector('main').appendChild(canvas)
+  canvas.width = length * 64
+  canvas.height =  64
 
-console.log(row,column)
+  const ctx = canvas.getContext('2d')
 
+  ctx.fillStyle = '#000000'
+  ctx.fillRect(0,0,canvas.width,canvas.height)
+  ctx.fillStyle = '#ffffff'
 
-const Plane = new PlaneGeometry(size,size,1,1)
-const position = new Float32Array(instances * 3)
-const material = getMaterial()
+  ctx.font = '40px Arial'
+  ctx.textAlign = 'center'
+  ctx.textBaseline = 'middle'
 
-const Mesh = new THREE.InstancedMesh(
-  Plane,
-  material,
-  instances
-)
+  for(let i = 0; i < length; i++) {
+    ctx.fillText(ascii[i],32 + i * 64,32)
+  }
 
-for(let r = 0; r < row; r++) {
-  for(let c = 0; c < column; c++) {
-    const index = (r * column) + c;
+  return {ascii:new THREE.CanvasTexture(canvas),length}
+};
 
-    const matrix = new THREE.Matrix4()
-    position[index + 0] = (c - column / 2) * size
-    position[index + 1] = (r - row / 2) * size 
-    position[index + 2] = 0 
+let size = 0.1;
 
-    matrix.setPosition((c - column / 2) * size,(r - row / 2) * size,0)
+const row = Math.ceil(height / size) + 1;
+const column = Math.ceil(width / size) + 1;
+const instances = row * column;
 
-    Mesh.setMatrixAt(index,matrix)
-    
+const Plane = new PlaneGeometry(size, size, 1, 1);
+const position = new Float32Array(instances * 3);
+const ScreenUVs = new Float32Array(instances * 2);
 
+const material = getMaterial({
+  invr: 1 / row,
+  invc: 1 / column,
+  person: Texture.load(Person),
+  ...GetASCIITexture()
+});
+
+const Mesh = new THREE.InstancedMesh(Plane, material, instances);
+
+for (let r = 0; r < row; r++) {
+  for (let c = 0; c < column; c++) {
+    const index = r * column + c;
+
+    const matrix = new THREE.Matrix4();
+    position[index + 0] = (c - column / 2) * size;
+    position[index + 1] = (r - row / 2) * size;
+    position[index + 2] = 0;
+
+    const u = c / column;
+    const v = r / row;
+
+    ScreenUVs[index * 2 + 0] = u;
+    ScreenUVs[index * 2 + 1] = v;
+
+    matrix.setPosition((c - column / 2) * size, (r - row / 2) * size, 0);
+
+    Mesh.setMatrixAt(index, matrix);
   }
 }
 
+// Attach as instance attribute
+Plane.setAttribute(
+  "screenUV",
+  new THREE.InstancedBufferAttribute(ScreenUVs, 2)
+);
+
 Mesh.instanceMatrix.needsUpdate = true;
 
-scene.add(Mesh)
+scene.add(Mesh);
 
-
-
-
-function Animate(){
-  renderer.renderAsync(scene,camera)
-  requestAnimationFrame(Animate)
+function Animate() {
+  renderer.renderAsync(scene, camera);
+  requestAnimationFrame(Animate);
 }
 
-requestAnimationFrame(Animate)
+requestAnimationFrame(Animate);
 
-
-function resize(){
-  camera.aspect = innerWidth/innerHeight
-  camera.updateProjectionMatrix()
+function resize() {
+  camera.aspect = innerWidth / innerHeight;
+  camera.updateProjectionMatrix();
   canvas.width = innerWidth;
   canvas.height = innerHeight;
-  renderer.setSize(innerWidth,innerHeight)
+  renderer.setSize(innerWidth, innerHeight);
 }
 
-window.addEventListener('resize',resize)
+window.addEventListener("resize", resize);
