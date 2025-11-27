13cec9151a74e930aa32c44f2993762e503934fe
Manishbhai9350 <keshraram294@gmail.com>
2025-11-27 16:07:22 +0530

    Final Effect

--- src/ascii/index.js
+++ src/ascii/index.js
@@ -1,77 +1,86 @@
 import {
-  float, vec2, vec3, vec4,
-  color, texture, uniform,
-  positionLocal, normalLocal,
-  uv, time,
+  float,
+  vec2,
+  vec3,
+  vec4,
+  color,
+  texture,
+  uniform,
+  positionLocal,
+  normalLocal,
+  uv,
+  time,
   attribute,
   Fn,
   pow,
   floor,
   lengthSq,
   mix,
-  step
-} from 'three/tsl';
+  step,
+  cos,
+  sin
+} from "three/tsl";
 
-import { MeshBasicNodeMaterial } from "three/webgpu"
+import { MeshBasicNodeMaterial } from "three/webgpu";
 
-const ColorPallete = [
-    '#FAF3E1',
-    '#D7C097',
-    '#4E56C0',
-    '#FF6D1F',
-    '#961E1E',
-]
-
-
+const ColorPallete = ["#b00ff5", "#ff33ebff", "#ff2976", "#ff901f", "#ffd318"];
 
 export const getMaterial = ({
-    invr=1,
-    invc=1,
-    person,
-    ascii,
-    length
+  invr = 1,
+  invc = 1,
+  person,
+  ascii,
+  length,
+  boxes,
 }) => {
-    const material = new MeshBasicNodeMaterial()
+  const material = new MeshBasicNodeMaterial();
 
-    const color1 = uniform(color(ColorPallete[0]))
-    const color2 = uniform(color(ColorPallete[2]))
-    const color3 = uniform(color(ColorPallete[2]))
-    const color4 = uniform(color(ColorPallete[3]))
-    const color5 = uniform(color(ColorPallete[4]))
+  const color1 = uniform(color(ColorPallete[0]));
+  const color2 = uniform(color(ColorPallete[2]));
+  const color3 = uniform(color(ColorPallete[2]));
+  const color4 = uniform(color(ColorPallete[3]));
+  const color5 = uniform(color(ColorPallete[4]));
 
+  material.positionNode = Fn(() => {
+    const nuv = uv().mul(2).add(1)
+  
     
-    material.colorNode = Fn(() => {
-    const RawScreenUV = attribute('screenUV', 'vec2');
-        const rows = attribute('rows', 'float');
-        const columns = attribute('column', 'float');
-        
-        const invRows = float(1).div(rows);
-        const invColumns = float(1).div(columns);
-    
-        const ScreenUV = RawScreenUV.add(uv().mul(vec2(invc,invr)))
+    return positionLocal
+  })()
 
-        const Person = texture(person,RawScreenUV)
+  material.colorNode = Fn(() => {
+    const RawScreenUV = attribute("screenUV", "vec2");
+    const rows = attribute("rows", "float");
+    const columns = attribute("column", "float");
 
-        const Brightness = pow(Person.r,.45)
-        let finalColor = color1;
-        finalColor = mix(finalColor,color2,step(.2,Brightness))
-        finalColor = mix(finalColor,color3,step(.4,Brightness))
-        finalColor = mix(finalColor,color4,step(.6,Brightness))
-        finalColor = mix(finalColor,color5,step(.8,Brightness))
+    const invRows = float(1).div(rows);
+    const invColumns = float(1).div(columns);
 
-        const asciiUV = vec2(
-            uv().x.
-            div(float(length))
-            .add(floor(pow(Person.r,1.5).mul(length)).div(length)),
-            uv().y)
-        const asciiTexture = texture(ascii,asciiUV)
+    const ScreenUV = RawScreenUV.add(uv().mul(vec2(invc, invr)));
 
-        
+    const Person = texture(person, RawScreenUV);
+    const Boxes = texture(boxes, RawScreenUV);
 
-        // return vec4(Brightness)
-        return asciiTexture.mul(finalColor)
-        // return vec4(vec3(finalColor),1)
-    })()
+    const Brightness = pow(Boxes.r, 1.2);
+    let finalColor = color1;
+    finalColor = mix(finalColor, color2, step(0.2, Brightness));
+    finalColor = mix(finalColor, color3, step(0.4, Brightness));
+    finalColor = mix(finalColor, color4, step(0.6, Brightness));
+    finalColor = mix(finalColor, color5, step(0.8, Brightness));
 
-    return material
-}
+    const asciiUV = vec2(
+      uv()
+        .x.div(float(length))
+        .add(floor(Brightness.mul(length)).div(length)),
+      uv().y,
+    );
+    const asciiTexture = texture(ascii, asciiUV);
+
+    // return texture(boxes, ScreenUV);
+    // return asciiTexture;
+    return asciiTexture.mul(finalColor);
+    // return vec4(vec3(finalColor), 1);
+  })();
+
+  return material;
+};
--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import { PlaneGeometry, WebGPURenderer } from "three/webgpu";
 import { GetSceneBounds } from "./utils";
 import { getMaterial } from "./ascii";
 import Person from "/person.jpg?url";
+import Natlie from "/natlie.png?url";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const { PI } = Math;
 
@@ -20,11 +22,41 @@ const scene = new THREE.Scene();
 
 const renderer = new WebGPURenderer({ canvas, antialias: true, alpha: true });
 
+// ?? Scene 2
+const renderTarget = new THREE.WebGLRenderTarget(innerWidth,innerHeight);
+const scene2 = new THREE.Scene();
+
+const Boxes = new THREE.Group()
+scene2.add(Boxes)
+
+const dl = new THREE.DirectionalLight(0xffffff, 3);
+dl.position.set(20,20,20);
+scene2.add(dl, new THREE.AmbientLight(0xffffff, .5));
+
+for(let i = 0; i < 100; i++) {
+  const x = (2 * Math.random() - 1) * 10;
+  const y = (2 * Math.random() - 1) * 10;
+  const z = -Math.random() * 20;
+
+  const Box = new THREE.Mesh(
+    new THREE.BoxGeometry(),
+    new THREE.MeshPhysicalMaterial({color:0xffffff})
+  )
+
+  Box.position.set(x,y,z)
+
+  Boxes.add(Box)
+
+
+}
+
+
+
 const camera = new THREE.PerspectiveCamera(
   75,
   innerWidth / innerHeight,
   1,
-  1000
+  1000,
 );
 camera.position.z = 5;
 
@@ -33,6 +65,7 @@ const Draco = new DRACOLoader(Manager);
 const Texture = new THREE.TextureLoader(Manager);
 const GLB = new GLTFLoader(Manager);
 
+
 Draco.setDecoderPath("/draco/");
 Draco.setDecoderConfig({ type: "wasm" });
 GLB.setDRACOLoader(Draco);
@@ -40,36 +73,41 @@ GLB.setDRACOLoader(Draco);
 const { width, height } = GetSceneBounds(renderer, camera);
 
 const GetASCIITexture = () => {
-  let ascii = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
+  let ascii =
+    "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
   const length = ascii.length;
 
   // document.querySelector('canvas').style.display = 'none'
-  const canvas = document.createElement('canvas')
+  const canvas = document.createElement("canvas");
   // document.body.querySelector('main').appendChild(canvas)
-  canvas.width = length * 64
-  canvas.height =  64
+  canvas.width = length * 64;
+  canvas.height = 64;
 
-  const ctx = canvas.getContext('2d')
+  const ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = '#000000'
-  ctx.fillRect(0,0,canvas.width,canvas.height)
-  ctx.fillStyle = '#ffffff'
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#ffffff";
 
-  ctx.font = '40px Arial'
-  ctx.textAlign = 'center'
-  ctx.textBaseline = 'middle'
+  ctx.font = "40px Arial";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
 
-  for(let i = 0; i < length; i++) {
-    ctx.fillText(ascii[i],32 + i * 64,32)
+  for (let i = 0; i < length; i++) {
+    if (i > length * 0.7) {
+      ctx.filter = "blur(2px)";
+    }
+    ctx.fillText(ascii[i], 32 + i * 64, 32);
+    ctx.filter = "none";
   }
 
-  return {ascii:new THREE.CanvasTexture(canvas),length}
+  return { ascii: new THREE.CanvasTexture(canvas), length };
 };
 
-let size = 0.1;
+let size = 0.15;
 
 const row = Math.ceil(height / size) + 1;
-const column = Math.ceil(width / size) + 1;
+const column = Math.ceil(row);
 const instances = row * column;
 
 const Plane = new PlaneGeometry(size, size, 1, 1);
@@ -79,8 +117,9 @@ const ScreenUVs = new Float32Array(instances * 2);
 const material = getMaterial({
   invr: 1 / row,
   invc: 1 / column,
-  person: Texture.load(Person),
-  ...GetASCIITexture()
+  person: Texture.load(Natlie),
+  ...GetASCIITexture(),
+  boxes: renderTarget.texture,
 });
 
 const Mesh = new THREE.InstancedMesh(Plane, material, instances);
@@ -90,8 +129,8 @@ for (let r = 0; r < row; r++) {
     const index = r * column + c;
 
     const matrix = new THREE.Matrix4();
-    position[index + 0] = (c - column / 2) * size;
-    position[index + 1] = (r - row / 2) * size;
+    position[index + 0] = (c - (column - 1) / 2) * size;
+    position[index + 1] = (r - (row - 1) / 2) * size;
     position[index + 2] = 0;
 
     const u = c / column;
@@ -109,14 +148,34 @@ for (let r = 0; r < row; r++) {
 // Attach as instance attribute
 Plane.setAttribute(
   "screenUV",
-  new THREE.InstancedBufferAttribute(ScreenUVs, 2)
+  new THREE.InstancedBufferAttribute(ScreenUVs, 2),
 );
 
 Mesh.instanceMatrix.needsUpdate = true;
 
 scene.add(Mesh);
 
-function Animate() {
+const controls = new OrbitControls(camera, canvas);
+
+const clock = new THREE.Clock();
+let Time = clock.getElapsedTime();
+
+function Animate(t) {
+  const NewTime = clock.getElapsedTime();
+  const DT = NewTime - Time;
+  Time = NewTime;
+  controls.update();
+
+
+  Boxes.children.forEach(Box => {
+    Box.rotation.x +=  DT * .2
+    Box.rotation.y +=  DT * .2
+    Box.rotation.z +=  DT * .2
+  })
+
+  renderer.setRenderTarget(renderTarget);
+  renderer.renderAsync(scene2, camera);
+  renderer.setRenderTarget(null);
   renderer.renderAsync(scene, camera);
   requestAnimationFrame(Animate);
 }
